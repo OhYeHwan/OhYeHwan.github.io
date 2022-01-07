3c9890eb18eb69179c09e5097f32ddb12574374d
OhYeHwan <azss0470@gmail.com>
2022-01-07 23:01:49 +0900

    init

---
+++ blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "Olog",
+  description: "기록의 중요성을 깨닫고 글을 쓰기 시작했습니다.",
+  author: "예환",
+  siteUrl: "https://OhYeHwan.github.io",
+  links: {
+    github: "https://github.com/OhYeHwan",
+    // linkedIn: "https://linkedin.com",
+    // facebook: "https://www.facebook.com",
+    // instagram: "https://www.instagram.com",
+    email: "azss0470@gmail.com",
+  },
+  utterances: {
+    repo: "OhYeHwan/OhYeHwan.github.io",
+    type: "pathname",
+  },
+}
---
+++ contents/posts/test-kr/index.md
@@ -0,0 +1,11 @@
+---
+title: "글 작성 테스트 입니다"
+description:
+date: 2022-01-07
+update: 2022-01-07
+tags:
+  - test
+series: ""
+---
+
+글 작성 테스트 입니다!
